abbe061ea4231d32913bb679125efb55d9a68c65
Carlos Escutia <carlosescutia@gmail.com>
2023-08-17 15:38:41 -0600

    Se agrega área pública.

---
+++ application/views/templates/footer.php
@@ -0,0 +1,33 @@
+        </main>
+
+        <div class="footer_margin">
+        </div>
+
+        <footer class="footer">
+            <ul class="nav justify-content-center">
+                <li class="nav-item">&copy; 2023 Organización</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item">·</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item"><i class="bi bi-telephone"></i> (123) 456 7890 &nbsp;</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item">·</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item">&nbsp;</li>
+                <li class="nav-item"><i class="bi bi-envelope"></i> contacto@organizacion.inet &nbsp; </li>
+            </ul>
+        </footer>
+        
+    </body>
+</html>
---
+++ application/views/templates/pubheader.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" href="<?=base_url()?>img/favicon.png" sizes="16x16" type="image/png" />
+
+        <title>Lorem Ipsum</title>
+
+        <!-- global css -->
+        <link rel="stylesheet" type="text/css" href="<?=base_url()?>css/publico.css" />
+
+        <!-- bootstrap 5.3 -->
+        <link href="<?=base_url()?>css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="<?=base_url()?>css/bootstrap-icons.css" rel="stylesheet"/>
+        <script src="<?=base_url()?>js/bootstrap.bundle.min.js"></script>
+
+        <!-- jquery -->
+        <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-sm navbar-light fixed-top d-print-block pr-3">
+            <div class="col-sm-8 offset-sm-2 text-center">
+                <div class="row mt-0 mb-0 pt-0 pb-0">
+                    <div class="col-sm-12 text-center">
+                    <img src="<?=base_url()?>img/banner_desktop.jpg" class="img-fluid align-top d-none d-sm-inline-block" alt="encabezado">
+                    <img src="<?=base_url()?>img/banner_movil.jpg" class="img-fluid align-top d-block d-sm-none" alt="encabezado">
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <main role="main" class="container-fluid col-sm-8 offset-sm-2">
